16e5f6d8f0f9d9bbdc83b283fd8546b16ea59300
Craig Swanson <cswanson02@me.com>
2020-03-18 18:21:24 -0500

    Adjusted UI
    Added larger button images

--- LambdaTimeline/View Controllers/VideoPostViewController.swift
+++ LambdaTimeline/View Controllers/VideoPostViewController.swift
@@ -15,7 +15,6 @@ class VideoPostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func startStopRecording(_ sender: UIButton) {
