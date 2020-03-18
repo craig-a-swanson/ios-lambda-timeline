d33be7c407afdcdad07b823f41fa555ee7e59728
Craig Swanson <cswanson02@me.com>
2020-03-18 17:58:57 -0500

    Added PostVideoVC
    Added the VideoPostViewController file and to the storyboard. Wired it up.

---
+++ LambdaTimeline/View Controllers/VideoPostViewController.swift
@@ -0,0 +1,34 @@
+//
+//  VideoPostViewController.swift
+//  LambdaTimeline
+//
+//  Created by Craig Swanson on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class VideoPostViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startStopRecording(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
