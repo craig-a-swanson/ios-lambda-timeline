53fcb68bbde0d118db7ded57b36d02d80902146a
Craig Swanson <cswanson02@me.com>
2020-03-18 20:41:33 -0500

    Added user's locatiion on map

---
+++ LambdaTimeline/Helpers/CameraPreviewView.swift
@@ -0,0 +1,28 @@
+//
+//  CameraPreviewView.swift
+//  LambdaTimeline
+//
+//  Created by Craig Swanson on 3/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    
+    // defaults to the CALayer class but we can override
+    // this layer can display it effectively
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPlayerLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPlayerLayer.session }
+        set { videoPlayerLayer.session = newValue }
+    }
+}
